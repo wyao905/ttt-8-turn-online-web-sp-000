29f444ca16ad666a9fd74c93bf81300b76e22fca
Warren Yao <warrenyca905@gmail.com>
2019-05-09 02:30:00 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,7 +3,20 @@ def display_board(board, index)
 end
 
 def valid_move?(board, index)
-  
+  if !position_taken?(board, index) && index.between?(0, 8)
+    return true
+  else
+    return false
+  end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
 end
 
 def move(board, index, token = "X")
