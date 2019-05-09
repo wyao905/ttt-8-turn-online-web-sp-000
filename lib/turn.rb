33cf4a6ff4b3cdf6cd4970756acc5450229af97a
Warren Yao <warrenyca905@gmail.com>
2019-05-09 02:30:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,7 +10,6 @@ def valid_move?(board, index)
   end
 end
 
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
