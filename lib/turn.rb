511bc16924e04876e4bb2e20c7353d220208a055
Warren Yao <warrenyca905@gmail.com>
2019-05-09 02:50:51 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,8 +23,8 @@ def valid_move?(board, index)
 end
 
 def input_to_index(user_input)
-  index = gets.user_input
-  return index.to_i
+  user_input = gets
+  return user_input.to_i
 end
 
 def move(board, index, token = "X")
