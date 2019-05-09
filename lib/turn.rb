a327f88b3db19563c4d801a32f8c6db40a974e41
Warren Yao <warrenyca905@gmail.com>
2019-05-09 02:39:01 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,14 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
 def valid_move?(board, index)
   if !position_taken?(board, index) && index.between?(0, 8)
     return true
@@ -14,12 +22,8 @@ def valid_move?(board, index)
   end
 end
 
-def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    return false
-  else
-    return true
-  end
+def input_to_index(user_input)
+  return user_input.get
 end
 
 def move(board, index, token = "X")
